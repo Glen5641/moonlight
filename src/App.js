176834cc132ba28efc79873791b3c481a96d5e0d
Chase <glen5641@gmail.com>
2021-06-08 15:27:06 -0500

    AddedMobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import BodyContainer from './Components/BodyContainer';
+import MetaTags from 'react-meta-tags';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <MetaTags>
+        <title>Moonlight Machine</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </MetaTags>
       <BodyContainer />
     </div>
   );
